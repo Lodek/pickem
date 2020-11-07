e6c1e856037d2dadde9bdd4d4b85d547f5968456
Lodek <23224552+Lodek@users.noreply.github.com>
2020-11-07 13:11:46 -0300

    references init

--- src/tree.rs
+++ src/tree.rs
@@ -3,10 +3,10 @@ use std::str;
 
 ///Encapsulates the data stored by a leaf
 struct LeafData {
-    name: str,
-    desc: str,
-    chord: str,
-    value: str
+    name: String,
+    desc: String,
+    chord: String,
+    value: String
 }
 
 ///Tree is a recursive data type with two forms: `Node` and `Leaf`.
@@ -18,7 +18,7 @@ enum Tree {
 }
 
 ///Returns map of children 1st level transitions for a tree.
-fn children(tree: Tree) -> HashMap<str, Tree> {
+fn children(tree: &Tree) -> HashMap<String, Tree> {
     match tree {
         Tree::Leaf(_) => HashMap::new(),
         Tree::Node(_, children) => {
@@ -31,7 +31,7 @@ fn children(tree: Tree) -> HashMap<str, Tree> {
 }
 
 ///Attempts to return a child of `Tree` whose chord is `chord`.
-fn transition(tree: Tree, chord: str) -> Option<Tree> {
+fn transition(tree: &Tree, chord: &String) -> Option<Tree> {
     let children_map = children(tree);
     children_map.get(chord);
 }
@@ -54,7 +54,7 @@ impl Picks {
 
     //Picks a child from `tree` based on the chord property.
     //Option is empty if no child was found for the given chord.
-    //fn pick(&picks: Picks, tree: Tree, chord: str) -> Option<Tree> {}
+    //fn pick(&picks: Picks, tree: Tree, chord: String) -> Option<Tree> {}
 
 
     //Undo the previous pick operation
@@ -63,7 +63,7 @@ impl Picks {
 
     //Returns list of values from all picked trees.
     //Result is sorted chronologically
-    //fn get_values(&picks: Picks) -> Vec<str> {}
+    //fn get_values(&picks: Picks) -> Vec<String> {}
 
     //Return list of trees pick. Result is sorted chronologically
     //fn get_trees(&picks: Picks) -> Vec<Tree> {} 
@@ -77,40 +77,40 @@ mod tests {
     #[test]
     fn children_from_node_returns_transitions() {
         let c1 = Tree::Leaf(LeafData {
-            name: "c1",
-            desc: "c1",
-            chord: "c1",
-            value: "c1"
+            name: String::from("c1")
+            desc: String::from("c1"),
+            chord: String::from("c1"),
+            value: String::from("c1")
         });
 
         let c2 = Tree::Leaf(LeafData {
-            name: "c2",
-            desc: "c2",
-            chord: "c2",
-            value: "c2"
+            name: String::from("c2"),
+            desc: String::from("c2"),
+            chord: String::from("c2"),
+            value: String::from("c2")
         });
 
         let parent = Tree::Node(LeafData {
-            name: "p",
-            desc: "p",
-            chord: "p",
-            value: "p"
+            name: String::from("p"),
+            desc: String("p"),
+            chord: String::from("p"),
+            value: String::from("p")
         }, vec![c1, c2]);
 
-        let children_map = children(parent);
-        assert_eq!(children_map.get("c1"), c1);
-        assert_eq!(children_map.get("c2"), c2);
+        let children_map = children(&parent);
+        assert_eq!(children_map.get(String::from("c1")), c1);
+        assert_eq!(children_map.get(String::from("c2")), c2);
     }
 
     #[test]
     fn children_from_node_returns_transitions() {
         let leaf = Tree::Leaf(LeafData {
-            name: "c1",
-            desc: "c1",
-            chord: "c1",
-            value: "c1"
+            name: String::from("c1"),
+            desc: String::from("c1"),
+            chord: String::from("c1"),
+            value: String::from("c1")
         });
-        let children_map = children(leaf);
+        let children_map = children(&leaf);
         assert_eq!(children_map.len(), 0);
     }
 
