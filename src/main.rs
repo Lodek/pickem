7f74c250435dceb76e414e65431e92079dd59dbb
Lodek <23224552+Lodek@users.noreply.github.com>
2020-11-15 19:28:42 -0300

    cleaned up main, still broken though

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,11 @@ use termion::event::Key;
 use std::io::{Write, stdin, stdout};
 use std::io;
 
-
-/// Converts the selected trees and lingering characters into a
-/// representative string.
-fn repr_input(trees: &Vec<&Tree>, input_buffer: &String) -> String {
-    let chords_selected = trees.iter().map(|tree| tree.data().chord).collect::<Vec<>>();
-    let user_input = chords_selected.join(" > ");
-    format!("{} > {}", user_input, input_buffer)
-}
-
-///Returns formatted string with the name of the selected trees
-///separated by " > ".
-fn repr_nodes(trees: &Vec<&Tree>) -> String {
-    trees.iter()
-        .map(|tree| tree.data().name)
-        .collect::<Vec<>>()
-        .join(" > ")
-}
-
-///Returns string of a choice formatted with colors for the terminal
-fn format_choice(tree: &Tree) -> String {
-    let data = tree.data();
-    format!("{}{}{} - {}",
-           color::Fg(color::Red),
-           data.chord,
-           color::Fg(color::Reset),
-           data.name)
-}
+use pickem::tree::Tree;
 
 
 ///Redraw function for each input
-fn redraw<T: Write>(file: &mut T, trees: &Vec<&Tree>, input_buffer: &String) -> io::Result<()> {
+fn redraw<T: Write>(file: &mut T, trees: Vec<&Tree>, input_buffer: &String) -> io::Result<()> {
     let root = trees.last().unwrap();
     let transitions = root.transitions_by_prefix(input_buffer.as_str());
     let formatted_transitions = transitions.values()
@@ -55,27 +29,6 @@ fn redraw<T: Write>(file: &mut T, trees: &Vec<&Tree>, input_buffer: &String) -> 
         .and_then(|v| file.flush())
 }
 
-fn get_tree() -> Tree {
-
-}
-
-fn handle_input(input_buffer: &mut String,
-                picked_trees: &mut Vec<Tree>) -> io::Result<()>{
-    let root = picked_trees.last().unwrap();
-    match root.transition(input_buffer.as_str()) {
-        Option::Some(tree) => {
-            input_buffer = "";
-            picked_trees.push(tree);
-            io::Result::Ok(())
-        },
-        Option::None => {
-            if root.transitions_by_prefix(input_buffer).len() == 0 {
-                io::Result::Err("No possible transitions, exiting")
-            }
-            io::Result::Ok(())
-        }
-    }
-}
 
 fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
@@ -83,23 +36,24 @@ fn main() {
     let tree = get_tree();
 
     let mut picked_trees: Vec<&Tree> = Vec::new();
-    let mut input_buffer: String = Vec::new();
+    let input_buffer = String::new();
+    let mut result;
 
     for c in stdin().keys() {
         match c.unwrap() {
             Key::Char(c) => {
                 input_buffer.push(c);
                 match handle_input(&mut input_buffer, &mut picked_trees) {
-                    io::Result::Ok(_) => {
-                        redraw()
-                        continue
-                    }
-                    io::Result::Err(_) => break;
+                    PickedLeaf => {
+                        result = picked_trees.last().unwrap().data().value;
+                        break
+                    },
+                    PickedNode => redraw(&mut stdout, &mut picked_trees, &mut input_buffer),
+                    InvalidPath => break,
+                    NoOp => continue
                 }
             },
-            Key::Esc {
-                break
-            },
+            Key::Esc =>  break,
             _ => ()
         }
     }
