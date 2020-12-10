980fdbf8f4c8dcdcbd6823e18733359a407a28cc
Lodek <23224552+Lodek@users.noreply.github.com>
2020-12-09 21:37:19 -0300

    change zsh widget

--- shell/pickem-widget.zsh
+++ shell/pickem-widget.zsh
@@ -1,9 +1,9 @@
-function pickem-widget() {
-   LBUFFER="${LBUFFER}$(cat example.yml | cargo run)"
+function widget-pickem-from-filename() {
+   LBUFFER="${LBUFFER}$(cat ~/.pickem/shell.yml | pickem)"
    local ret=$?
    zle reset-prompt
    return $ret
 }
 
-zle -N pickem-widget
-bindkey '^N' pickem-widget
+zle -N widget-pickem-from-filename
+bindkey '^N' widget-pickem-from-filename
