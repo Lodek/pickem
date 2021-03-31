74e7c85076670e83dd0b871a45a999de99659124
Bruno Gomes <23224552+Lodek@users.noreply.github.com>
2021-03-30 21:58:18 -0300

    feat: WIP Controller / View impl

--- src/tui.rs
+++ src/tui.rs
@@ -2,7 +2,7 @@ use super::driver::{Driver, DriverSignal};
 use super::util;
 use super::tree::Tree;
 
-use std::io::{Result, Write};
+use std::io::{Result, Write, ErrorKind};
 use std::fs::{OpenOptions, File};
 
 use termion;
@@ -17,10 +17,16 @@ use termios;
 #[derive(PartialEq)]
 pub enum Flags {
     QuitDeadEnd,
-    PrintFullValuePath,
-    PrintNodes,
     LoopMode,
-    LeafToggle
+    LeafToggle,
+}
+
+// TODO Think of a better way to handle different print modes
+// Is a enum really the best way?
+pub enum PrintMode {
+    FullPathValue,
+    SingleValue,
+    ParentChild,
 }
 
 
@@ -32,71 +38,78 @@ pub struct Controller {
 
 impl Controller {
 
-    pub fn new(tree: Tree) -> Result<Controller> {
+    pub fn new(driver: Driver, view: View) -> Result<Controller> {
         Ok(
             Self {
-            driver: Driver::new(tree),
-            view: View(),
+            driver: driver,
+            view: view,
             keys: termion::async_stdin().keys(),
         })
     }
 
-    /// Awaits user inputs until an break condition occurs or an exit signal is received
+    fn handle_input(&self, key: Key) -> Result<bool> {
+        match key {
+            Key::Backspace => {
+                let signal = self.driver.drive(DriverCommand::Backtrack);
+                self.view.update(signal).map(|_| true)
+            },
+            Key::Char(c) => {
+                let signal = self.driver.drive(DriverCommand::Backtrack);
+                self.view.update(signal).map(|_| true)
+            },
+            Key::Esc | Key::Char('\n') =>  Result::Ok(false),
+            _ => Result::Ok(true),
+        }
+    }
+
+    /// Iterate over user inputs, handling each one. An `Ok(false)` means run should 
+    /// return, `Ok(true)` repeats the loop and an `Error` returns.
     pub fn run(&mut self) -> Result<()> {
-        self.redraw()?;
-        loop {
-            if let Some(key) = self.keys.next() {
-                match key.unwrap() {
-                    Key::Char('\n') => break,
-                    Key::Char(c) => {
-                        self.driver.send_char(c);
-                        let signal = self.driver.evaluate();
-                        match signal {
-                            DriverSignal::LeafPicked => {
-                                break
-                            },
-                            DriverSignal::NodePicked => (),
-                            DriverSignal::DeadEnd => break,
-                            DriverSignal::NoOp => (),
-                            DriverSignal::Popped => ()
-                        }
-                    },
-                    Key::Esc =>  break,
-                    _ => ()
-                }
-                self.redraw()?;
+        for key in self.keys {
+            match self.handle_input(key.unwrap()) {
+                Result::Ok(false) => return Result::Ok(()),
+                Result::Ok(true) => (),
+                err @ Result::Err(_) => return err,
             }
         }
-        Ok(())
     }
 }
 
 
+// TODO Move some of the helpers to a helper module for TUI  view
+// Maybe a view module with common functionalities + trait?
+// Perhaps a TUI module?
 
 /// NOTE: tty device is taken from stderr using the `/proc` directory in a linux system,
 /// which is to say, if stderr is redirected, interactive elements will be a bust.
-struct View {
+struct View<'a> {
     tty: File,
     backup_termios: Termios,
+    driver: &'a Driver
 }
 
-impl View {
+impl<'a> View<'a> {
 
     //This is a hack to make pickem play nice with input and output data.
     //Assuming stderr isn't redirected, fd 2 should always point to the tty itself
     //which means, it can be written to in order to redraw the terminal.
     const INTERFACE_FD: i32 = 2;
 
-    pub fn new() -> View {
-        let tty_file = format!("/dev/fd/{}", INTERFACE_FD);
+    pub fn new(driver: &Driver ) -> View {
+        let tty_file = format!("/dev/fd/{}", Self::INTERFACE_FD);
         let tty = OpenOptions::new().read(true).write(true).open(tty_file)?;
         let backup_termios = Termios::from_fd(Self::INTERFACE_FD)?;
         let view = View {
+            driver: driver,
             tty: tty,
             backup_termios: backup_termios,
-        }
+        };
         view.set_cbreak_mode();
         return view;
+    }
+
+    pub fn update(&self, signal: DriverSignal) -> Result<()> {
+
     }
 
     /// Sets the tty given by `fd` into cbreak_mode
@@ -113,10 +126,10 @@ impl View {
     }
 
     pub fn redraw(&mut self) -> Result<()> {
-        let root = self.driver.root();
-        let transitions = root.transitions_by_prefix(self.driver.input_buffer.as_str());
-        let mut transitions = transitions.values()
-            .map(|tree| util::pprint_choice(*tree))
+        let mut transitions = self.driver
+            .get_transitions()
+            .into_iter()
+            .map(|tree| util::pprint_choice(tree))
             .collect::<Vec<String>>();
         transitions.sort();
         let formatted_transitions = transitions.join("\n\r");
@@ -139,12 +152,6 @@ impl View {
             .and_then(|v| self.tty.flush())
     }
 
-    /// Routine that restores tty before exit
-    pub fn cleanup(&mut self) -> Result<()> {
-        self.reset_screen()?;
-        termios::tcsetattr(INTERFACE_FD, termios::TCSANOW, &self.backup_termios)
-    }
-
     /// Formats result and takes care of presenting it to user
     pub fn present_result(&mut self) -> Result<()> {
         self.reset_screen()?;
@@ -154,5 +161,13 @@ impl View {
             .join(" ");
         println!("{}", result.as_str());
         Ok(())
+    }
+}
+
+impl<'a> Drop for View<'a> {
+
+    /// Restore tty's termios settings
+    pub fn drop(&mut self) -> {
+        termios::tcsetattr(Self::INTERFACE_FD, termios::TCSANOW, &self.backup_termios)?;
     }
 }
