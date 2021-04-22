0ebf3fdef1c12b4aee380ed105f0425ae837a715
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-04-21 21:02:01 -0300

    refactor: broke up tui, fixed controller

--- src/tui.rs
+++ src/tui.rs
@@ -1,54 +1,43 @@
-use super::driver::{Driver, DriverSignal};
-use super::util;
-use super::tree::Tree;
-
 use std::io::{Result, Write, ErrorKind};
 use std::fs::{OpenOptions, File};
+
+use super::util;
+use super::driver::{Driver, DriverSignal, DriverCommand};
+use super::tree::Tree;
 
 use termion;
 use termion::AsyncReader;
 use termion::input::{TermRead, Keys};
 use termion::event::Key;
-
-use termios::Termios;
 use termios;
+use termios::Termios;
 
 
 #[derive(PartialEq)]
 pub enum Flags {
     QuitDeadEnd,
     LoopMode,
-    LeafToggle,
+    OutputOnPick,
 }
 
-// TODO Think of a better way to handle different print modes
-// Is a enum really the best way?
-pub enum PrintMode {
-    FullPathValue,
-    SingleValue,
-    ParentChild,
+pub struct Controller<'a> {
+    driver: Driver<'a>,
+    view: View<'a>,
 }
 
+impl<'a> Controller<'a> {
 
-pub struct Controller {
-    driver: Driver,
-    view: View,
-    keys: Keys<AsyncReader>,
-}
-
-impl Controller {
-
-    pub fn new(driver: Driver, view: View) -> Result<Controller> {
+    pub fn new(driver: Driver<'a>, view: View<'a>) -> Result<Controller<'a>> {
         Ok(
             Self {
             driver: driver,
             view: view,
-            keys: termion::async_stdin().keys(),
         })
     }
 
-    fn handle_input(&self, key: Key) -> Result<bool> {
+    fn handle_input(&mut self, key: Key) -> Result<bool> {
         match key {
+            Key::Esc | Key::Char('\n') =>  Result::Ok(false),
             Key::Backspace => {
                 let signal = self.driver.drive(DriverCommand::Backtrack);
                 self.view.update(signal).map(|_| true)
@@ -57,7 +46,6 @@ impl Controller {
                 let signal = self.driver.drive(DriverCommand::Backtrack);
                 self.view.update(signal).map(|_| true)
             },
-            Key::Esc | Key::Char('\n') =>  Result::Ok(false),
             _ => Result::Ok(true),
         }
     }
@@ -65,13 +53,15 @@ impl Controller {
     /// Iterate over user inputs, handling each one. An `Ok(false)` means run should 
     /// return, `Ok(true)` repeats the loop and an `Error` returns.
     pub fn run(&mut self) -> Result<()> {
-        for key in self.keys {
+        let keys = termion::async_stdin().keys();
+        for key in keys {
             match self.handle_input(key.unwrap()) {
                 Result::Ok(false) => return Result::Ok(()),
                 Result::Ok(true) => (),
-                err @ Result::Err(_) => return err,
+                Result::Err(err) => return Result::Err(err),
             }
         }
+        Ok(())
     }
 }
 
@@ -82,10 +72,10 @@ impl Controller {
 
 /// NOTE: tty device is taken from stderr using the `/proc` directory in a linux system,
 /// which is to say, if stderr is redirected, interactive elements will be a bust.
-struct View<'a> {
+struct View<'a> { //TODO try messing with the lifetimes here, add a lifetime b, rethink what those two lifetimes mean
     tty: File,
     backup_termios: Termios,
-    driver: &'a Driver
+    driver: &'a Driver<'a>,
 }
 
 impl<'a> View<'a> {
@@ -95,21 +85,17 @@ impl<'a> View<'a> {
     //which means, it can be written to in order to redraw the terminal.
     const INTERFACE_FD: i32 = 2;
 
-    pub fn new(driver: &Driver ) -> View {
+    pub fn new(driver: &'a Driver) -> Result<View<'a>> {
         let tty_file = format!("/dev/fd/{}", Self::INTERFACE_FD);
         let tty = OpenOptions::new().read(true).write(true).open(tty_file)?;
         let backup_termios = Termios::from_fd(Self::INTERFACE_FD)?;
-        let view = View {
-            driver: driver,
-            tty: tty,
-            backup_termios: backup_termios,
-        };
-        view.set_cbreak_mode();
-        return view;
+        let view = View { driver, tty, backup_termios };
+        view.set_cbreak_mode()?;
+        Result::Ok(view)
     }
 
-    pub fn update(&self, signal: DriverSignal) -> Result<()> {
-
+    pub fn update(&mut self, signal: DriverSignal) -> Result<()> {
+        self.redraw()
     }
 
     /// Sets the tty given by `fd` into cbreak_mode
@@ -122,52 +108,139 @@ impl<'a> View<'a> {
         cbreak_termios.c_oflag &= !termios::OPOST;
         cbreak_termios.c_cc[termios::VMIN] = 1;
         cbreak_termios.c_cc[termios::VTIME] = 0;
-        termios::tcsetattr(fd, termios::TCSANOW, &cbreak_termios)
+        termios::tcsetattr(Self::INTERFACE_FD, termios::TCSANOW, &cbreak_termios)
     }
 
-    pub fn redraw(&mut self) -> Result<()> {
+    fn redraw(&mut self) -> Result<()> {
         let mut transitions = self.driver
             .get_transitions()
             .into_iter()
             .map(|tree| util::pprint_choice(tree))
-            .collect::<Vec<String>>();
+            .collect::<Vec<_>>();
         transitions.sort();
         let formatted_transitions = transitions.join("\n\r");
 
         write!(self.tty, "{}{}{}{}{}{}{}",
                termion::clear::All,
                termion::cursor::Goto(1,1),
-               util::pprint_nodes(&self.driver.trees),
+               util::pprint_nodes(&self.driver.path()),
                termion::cursor::Goto(1,2),
-               util::pprint_user_input(&self.driver.trees, &self.driver.input_buffer.as_str()),
+               util::pprint_user_input(&self.driver.path(), &self.driver.input_buffer()),
                termion::cursor::Goto(1,4),
                formatted_transitions)
-            .and_then(|v| self.tty.flush())
-    }
-
-    pub fn clean(&mut self) -> Result<()> {
-        write!(self.tty, "{}{}",
-               termion::clear::All,
-               termion::cursor::Goto(1, 1))
-            .and_then(|v| self.tty.flush())
-    }
-
-    /// Formats result and takes care of presenting it to user
-    pub fn present_result(&mut self) -> Result<()> {
-        self.reset_screen()?;
-        let result: String = self.driver.trees.iter()
-            .map(|tree| tree.data().value.as_str())
-            .collect::<Vec<&str>>()
-            .join(" ");
-        println!("{}", result.as_str());
-        Ok(())
+            .and_then(|_| self.tty.flush())
     }
 }
 
 impl<'a> Drop for View<'a> {
-
     /// Restore tty's termios settings
-    pub fn drop(&mut self) -> {
-        termios::tcsetattr(Self::INTERFACE_FD, termios::TCSANOW, &self.backup_termios)?;
+    fn drop(&mut self) -> () {
+        termios::tcsetattr(Self::INTERFACE_FD, termios::TCSANOW, &self.backup_termios).unwrap();
     }
+}
+
+struct OutputView<'a> {
+    of: File,
+    driver: &'a Driver<'a>,
+    format: OutputFormat,
+}
+
+enum OutputFormat {
+    Value,
+    Signal
+}
+
+impl<'a> OutputView<'a> {
+
+    pub fn new(driver: &'a Driver<'a>, format: OutputFormat) -> Result<OutputView<'a>> {
+        let path = "/dev/fd/1"; //stdout
+        let of = OpenOptions::new().read(false).write(true).open(path)?;
+        Ok(OutputView {
+            of: of,
+            driver: driver,
+            format: format,
+        })
+    }
+
+    /// Formats result and takes care of presenting it to user
+    pub fn update(&mut self, signal: DriverSignal) -> Result<()> {
+        match signal {
+            DriverSignal::NodePicked(tree) | DriverSignal::LeafPicked(tree) => write!(self.of, "{}", tree.data().value),
+            _ => Ok(())
+        }
+    }
+}
+
+mod view_helpers {
+
+    use termion::{color, AsyncReader};
+    use super::super::tree::Tree;
+
+    /// Converts the selected trees and lingering characters into a
+    /// representative string.
+    pub fn pprint_user_input(trees: &Vec<&Tree>, input_buffer: &str) -> String {
+        let chords_selected = trees.iter().map(|tree| tree.data().chord.as_str()).collect::<Vec<&str>>();
+        let user_input = chords_selected.join(" > ");
+        format!("{} > {}", user_input, input_buffer)
+    }
+
+
+    ///Returns formatted string with the name of the selected trees
+    ///separated by " > ".
+    pub fn pprint_nodes(trees: &Vec<&Tree>) -> String {
+        trees.iter()
+            .map(|tree| tree.data().name.as_str())
+            .collect::<Vec<_>>()
+            .join(" > ")
+    }
+
+    ///Returns string of a choice formatted with colors for the terminal
+    pub fn pprint_choice(tree: &Tree) -> String {
+        let data = tree.data();
+        format!("{}{}{} - {}",
+               color::Fg(color::Red),
+               data.chord,
+               color::Fg(color::Reset),
+               data.name)
+    }
+
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_pprint_user_input() {
+            let data = LeafData {
+                name: String::from("name"),
+                desc: String::from("desc"),
+                chord: String::from("chord"),
+                value: String::from("value"),
+            };
+            let tree = Tree::Leaf(data);
+            let trees: Vec<&Tree> = vec![&tree];
+            assert_eq!(pprint_user_input(&trees, &"a"), String::from("chord > a"));
+        }
+
+        #[test]
+        fn test_pprint_nodes() {
+            let d1 = LeafData {
+                name: String::from("root"),
+                desc: String::from("root"),
+                chord: String::from("root"),
+                value: String::from("root"),
+            };
+            let d2 = LeafData {
+                name: String::from("programs"),
+                desc: String::from("programs"),
+                chord: String::from("programs"),
+                value: String::from("programs"),
+            };
+            let root = Tree::Leaf(d1);
+            let programs = Tree::Leaf(d2);
+            let trees: Vec<&Tree> = vec![&root, &programs];
+            assert_eq!(pprint_nodes(&trees), String::from("root > programs"));
+        }
+    }
+
 }
