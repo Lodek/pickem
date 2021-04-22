40ec347b5707e2770d511363ca1c5ce66a869594
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-04-21 22:26:17 -0300

    refactor: view now receives reference to signal

--- src/front_end/traits.rs
+++ src/front_end/traits.rs
@@ -1,9 +1,11 @@
 use super::driver::{Driver, DriverSignal, DriverCommand};
 
 pub trait View {
-    fn update(&mut self, signal: DriverSignal) -> Result<()>;
+    fn update(&mut self, signal: &DriverSignal) -> Result<()>;
 }
 
 pub trait Controller {
+    /// Entrypoint for controller instance. Method should run until
+    /// pickem execution is complete.
     pub fn run(&mut self) -> Result<()>;
 }
